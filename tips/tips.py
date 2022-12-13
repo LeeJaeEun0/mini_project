801a3613655889380bcc2777563cd9da311b3612
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-13 17:24:40 +0900

    change_pro

---
+++ tips/tips.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("서울 통근, 통학하는 교통수단")
+
+st.write(
+   "https://data.seoul.go.kr/dataList/10283/S/2/datasetView.do"
+)
+
+# UTF-8 / CP-949
+# https://seong6496.tistory.com/269
+df = pd.read_csv('./commute_seoul/data.csv', encoding='cp949')
+st.write(df)
