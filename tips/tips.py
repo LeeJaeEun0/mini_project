603a909aedcecc484f1a80afa198797446703ff3
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-13 18:18:42 +0900

    update_test

--- tips/tips.py
+++ tips/tips.py
@@ -4,11 +4,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("tips")
-
 tips = sns.load_dataset('tips')
 # st.write(tips)
+st.write(
+    '''
+    # tips 데이터셋을 이용한 데이터 시각화
+    ## tips 데이터
+    '''
+)
+st.write(tips.head)
 
 fig = plt.figure(figsize=(8, 4))
 sns.histplot(data=tips, x='size')
 st.pyplot(fig)
+
