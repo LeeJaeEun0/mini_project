38ab6be769fffb2bb2e94267ebcb64c8363f54d7
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-13 16:04:06 +0900

    update_con

--- commute/commute_seoul.py
+++ commute/commute_seoul.py
@@ -4,4 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("제목 입니다.")
+st.title("서울 통근, 통학하는 교통수단")
+
+st.write(
+   "https://data.seoul.go.kr/dataList/10283/S/2/datasetView.do"
+)
+
+# UTF-8 / CP-949
+# https://seong6496.tistory.com/269
+df = pd.read_csv('./cmmute_seoul/data.csv', encoding='cp949')
+st.write(df)
