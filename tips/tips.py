e6e57aa9078105e99e3e0d5e8a89cc814b1b05dc
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-14 09:11:07 +0900

    update_first

--- tips/tips.py
+++ tips/tips.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 tips = sns.load_dataset('tips')
 # st.write(tips)
 st.write(
@@ -13,7 +12,6 @@ st.write(
     '''
 )
 st.write(tips)
-
 st.write(
     '''
     ### 총액의 빈도수
@@ -22,26 +20,6 @@ st.write(
 fig = plt.figure(figsize=(8, 4))
 sns.histplot(data=tips, x='total_bill')
 st.pyplot(fig)
-
-st.write( 
-    '''
-    ### 총액 중 팁의 비율
-    '''
-)
-fig1 = plt.figure(figsize=(8, 4))
-sns.kdeplot(data=tips, x='total_bill')
-sns.rugplot(data=tips, x='tip')
-st.pyplot(fig1)
-
-st.write( # 갑작스럽게 이상하게 보임
-    '''
-    ### 인원수와 팁의 상관관계
-    '''
-)
-fig = plt.figure(figsize=(8, 4))
-sns.boxplot(x='total_bill', y='tip', data=tips)
-st.pyplot(fig)
-
 st.write(
     '''
     ### 식사 인원수
@@ -51,11 +29,19 @@ fig = plt.figure(figsize=(8, 4))
 sns.histplot(data=tips, x='size')
 st.pyplot(fig)
 
-st.write( # 갑자기 이상하게 출려 ㅜ
+st.write(
     '''
-    ### 인원수에 따른 분석
+    ### 인원수와 팁의 상관관계
     '''
 )
+fig = plt.figure(figsize=(8, 4))
+sns.boxplot(x='size', y='total_bill', data=tips)
+st.pyplot(fig)
 
+st.write(
+    '''
+    ### 인원수에 따른 그래프
+    '''
+)
 fig = sns.pairplot(tips, hue="size")
 st.pyplot(fig)
